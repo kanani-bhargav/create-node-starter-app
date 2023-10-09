b671424248fa4da2c0d99ea2b94d291e28ce9ef5
BHARGAV KANANI <kananibhargavkd11@gmail.com>
2023-10-09 19:43:15 +0530

    initial commit

--- bin/delete.js
+++ bin/delete.js
@@ -28,7 +28,7 @@ function deleteDirectoryRecursive(directoryPath) {
       }
     });
     // Delete the empty directory itself
-    fs.rm(directoryPath);
+    fs.rm(path, { recursive: true })
   } else {
     console.error(`Directory does not exist: ${directoryPath}`);
   }
