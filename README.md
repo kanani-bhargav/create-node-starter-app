b7856803ee4c2f37e5ab2232bd66149a5611fcfe
BHARGAV KANANI <kananibhargavkd11@gmail.com>
2023-10-30 10:10:04 +0530

    add feature

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# create-node-starter-app"
+## Getting Started with create-node-starter-app Starter App
+A comprehensive Node.js starter kit for building web applications using best practices and a well-structured project layout.
+
+
+## Table of Contents
+
+1. [Introduction]
+2. [Project Structure]
+3. [Features]
+4. [Prerequisites]
+5. [Installation]
+
+
+## 1. Introduction
+My Node.js starter app is a lightweight, opinionated project template for kickstarting Node.js applications. It's designed to simplify the process of setting up a new Node.js project by providing a pre-configured directory structure, essential dependencies, and a basic project skeleton.
+
+## 2. Project Structure
+
+Explain your project's structure to help users understand how different components are organized within your starter kit. You can use a tree structure or bullet points to describe the layout.
+
+[repo-name]/
+ ├── src/
+ │    ├── config/
+ │   │   └── config.js
+ │   ├── controllers/
+ │   │   └── user.controller.js
+ │   ├── db/
+ │   │   └──dbConnection.js
+ │   ├── helpers/
+ │   │   ├── crons.js
+ │   │   └── pick.js
+ │   ├── middleware/
+ │   │   ├── auth.js
+ │   │   ├── upload.js
+ │   │   └── validate.js
+ │   ├── models/
+ │   │   └── user.model.js
+ │   ├── routes/
+ │   │   └── user.route.js
+ │   ├── services/
+ │   │   └── user.service.js
+ │   ├── validations/
+ │   │   └── user.validation.js
+ │   └── index.js
+ ├── .env
+ ├── .gitignore
+ ├── .nvmrc
+ ├── package.json
+ └── README.md
+
+## 3. Features
+List the key features that your professional Node.js starter kit offers. Highlight what sets it apart from other solutions.
+   -mongooseSchema building
+   -auth
+   -image upload
+
+## 4. Prerequisites
+ This typically includes git,Node.js and npm.
+   -download and install git
+   -download and install Node.js
+
+## 5. Installation
+Explain how to set up and install your starter kit. Provide step-by-step instructions and commands for users to follow.
+  - create directory path
+  - In cli run command npx create-node-starter-app [your repository name]
--- bin/cli.js
+++ bin/cli.js
@@ -1,40 +1,44 @@
 #!/usr/bin/env node
-const {execSync} =require('child_process');
-const fs= require('fs')
-const path= require('path')
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
-const runCommand=command=>{
-try {
-    execSync(`${command}`,{stdio:'inherit'});
-} catch (error) {
-    console.log(`Faild to execute ${command}`,error);
-    return false
-}
-return true
-}
-const repoName=process.argv[2]
-if(!repoName){
-  console.log(`Please specify the project directory`);
-  process.exit(-1)
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (error) {
+    console.log(`Faild to execute ${command}`, error);
+    return false;
+  }
+  return true;
 };
-const gitCheckoutCommand=`git clone --depth 1 https://github.com/kanani-bhargav/create-node-starter-app ${repoName}`
-const installDepsCommand=` cd ${repoName} && npm install`;
+const repoName = process.argv[2];
+if (!repoName) {
+  console.log(`Please specify the project directory`);
+  process.exit(-1);
+}
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/kanani-bhargav/create-node-starter-app ${repoName}`;
+const installDepsCommand = ` cd ${repoName} && npm install`;
 
 console.log(`cloning the repository with name ${repoName}`);
-const checkedOut=runCommand(gitCheckoutCommand)
-if(!checkedOut)process.exit(-1);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
 
 console.log(`installing dependencies for ${repoName}`);
-const installedDeps =runCommand(installDepsCommand)
-if(!installedDeps)process.exit(-1);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
 
-console.log(`Congretulation ! you are ready. follow the following command to start`);
-console.log(`cd ${repoName} && npm start`);
+console.log(
+  `Congretulation ! you are ready. follow the following command to start`
+);
+
 console.log(`1. cd ${repoName}`);
-console.log(`2. open .env and fill all record`);
+console.log(`2. open .env and change detail according project`);
 console.log(`3. npm start`);
 
-runCommand(`node ./bin/delete.js`)
-
-
-
+if (repoName == ".") {
+  runCommand(`node ./bin/delete.js`);
+}else{
+  runCommand(`cd ${repoName}`);
+  runCommand(`node ./bin/delete.js`);
+}
